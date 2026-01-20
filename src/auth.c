058d00c2b5917aabb7fe31b3cc696f91942a6227
souvikvos <souvikgh.work@gmail.com>
2026-01-21 00:11:16 +0530

    Souvik: feat(added login and registration system) whole auth system completed

--- src/auth.c
+++ src/auth.c
@@ -3,7 +3,7 @@
 #include<string.h>
 #include "../include/auth.h"
 
-#define USERS_FILE "../data/users.dat"
+#define USERS_FILE "data/users.dat"
 
 void registerUser(){
     User u;
@@ -15,4 +15,66 @@ void registerUser(){
     scanf("%d", &u.user_id);
 
     printf("Enter Name: ");
+    scanf(" %39[^\n]", u.name); //reads full line including spaces
+
+    printf("Enter Password: ");
+    scanf(" %19s", u.password); //reads single word password
+
+    // set role automatically as user
+    u.role = ROLE_USER;
+
+    // open file in append binary mode
+    fp = fopen(USERS_FILE, "ab");
+
+    if(fp == NULL){
+        printf("Error: Unable to open users file!\n");
+        return;
+    }
+
+    // write structure to file
+    fwrite(&u, sizeof(User),1,fp);
+
+    fclose(fp);
+
+    printf("\n user Registered Successfully");
+}
+
+// Now writing function for loginuser()
+
+int loginUser(){
+    User u;
+    FILE *fp;
+
+    int id;
+    char pass[20];
+
+    printf("\n------------Login-------------\n");
+    printf("Enter User ID: ");
+    scanf("%d",&id);
+
+    printf("Enter password: ");
+    scanf(" %19s", pass);
+
+    // open users file in read binary mode
+    fp = fopen(USERS_FILE, "rb");
+
+    if(fp == NULL){
+        printf("Error: users file not found!\n");
+        return 0;
+    }
+
+    // read users records one by one
+    while(fread(&u,sizeof(User),1,fp)){
+        if(u.user_id == id && strcmp(u.password,pass) == 0){
+            fclose(fp);
+            printf("\nLogin Successful! Welcome %s\n",u.name);
+            return u.role; //ROLE_ADMIN or ROLE_USER
+        }
+    }
+
+    fclose(fp);
+    printf("\nLogin Failed! Wrong User ID or Password.\n");
+
+    return 0;
+
 }
--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../include/auth.h"
 
 void welcome_screen()
 {
@@ -19,8 +20,79 @@ int main_menu()
     printf("------------------------------");
     printf("Enter your choice: ");
     scanf("%d", &choice);
+
+    return choice;
 }
 
+
+
+void adminMenu(){
+    int choice;
+
+    while(1){
+        printf("\n==========ADMIN MENU===========\n");
+        printf("1. Vehicle Management\n");
+        printf("2. Service Records\n");
+        printf("3. Logout\n");
+        printf("==================================\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+
+    switch(choice){
+        case 1:
+        printf("\n[Vehicle module coming next]\n");
+        break;
+
+        case 2:
+        printf("\n[Service module coming next]\n");
+        break;
+        case 3:
+        printf("\nLogging out...\n");
+        return; // goes back to main menu
+
+        default:
+        printf("\nInvalid choice. Try again.\n");
+
+    }
+}
+}
+
+void userMenu(){
+    int choice;
+    while(1){
+        printf("\n=============USER MENU==============\n");
+        printf("1. View Service History\n");
+        printf("3.Logout\n");
+        printf("======================================\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+
+        switch(choice){
+            case 1:
+            printf("\n[View service history coming next]\n");
+            break;
+
+            case 2:
+            printf("\n[Search vehicle coming next]\n");
+            break;
+
+
+            case 3:
+            printf("\nLogging out...\n");
+            return;
+
+            default:
+            printf("\nInvalid choice. Try again.\n");
+            
+        }
+
+     }
+
+
+
+}
 int main()
 {
     welcome_screen();
@@ -32,10 +104,21 @@ int main()
         switch (choice)
         {
         case 1:
-            printf("login feature coming soon....\n");
+            int role = loginUser();
+
+            if(role ==  ROLE_ADMIN){
+                adminMenu();
+            }
+            else if(role == ROLE_USER){
+                userMenu();
+            }
+            else{
+                printf("\nLogin failed.Try again. \n");
+            }
+
             break;
         case 2:
-            printf("Register feature coming soon...\n");
+            registerUser();
             break;
         case 3:
             printf("Exiting..Good Bye!\n");
